fa80ac8f1af7fb0c04ef38c111376471cc0e857b
stephen fortune <steve.fortune@icecb.com>
2014-11-28 03:45:11 +0000

    Fixes #2. Wrote some brief additional comments for our new class to document why we've made some of the choices

--- Pod/Classes/LDBCollectionTransaction.h
+++ Pod/Classes/LDBCollectionTransaction.h
@@ -15,6 +15,17 @@
  
  Decorator class for LDBCollection that adds support for transaction-like commit / rollbacks.
  
+ This class uses `NSMutableOrderedSet`s to register changes to be commited in the transaction 
+ because:
+ 
+ - We want to be able to append objects on to the sets (mutable)
+ - We want to order of the changes to be applied in the order that they were registered (ordered)
+ - We don't want duplicate objects registered (set)
+
+ Because most foundation's mutable collection classes are not thread safe (and this may be used 
+ across multiple threads), public methods that mutate these sets (`insert`, `update`, `remove`, 
+ `commit` and `rollback`) implement mutex locks to avoid race conditions.
+ 
  @author Steve Fortune
  
  */
--- Pod/Classes/LDBCollectionTransaction.m
+++ Pod/Classes/LDBCollectionTransaction.m
@@ -74,7 +74,6 @@
         }
         
     }
-    
 }
 
 -(void) commit{
